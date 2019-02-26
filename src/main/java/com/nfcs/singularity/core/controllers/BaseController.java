e6a34f7eebad998e0eb5f2570f6713f9591a32d1
ArchiMage <archimagealex@gmail.com>
2019-02-26 10:51:28 +0300

    Huge BaseController refactoring

--- src/main/java/com/nfcs/singularity/core/controllers/BaseController.java
+++ src/main/java/com/nfcs/singularity/core/controllers/BaseController.java
@@ -1,11 +1,9 @@
 package com.nfcs.singularity.core.controllers;
 
 import com.nfcs.singularity.core.domain.BaseEntity;
-import com.nfcs.singularity.core.domain.Role;
-import com.nfcs.singularity.core.domain.User;
 import com.nfcs.singularity.core.generators.CRUDGenerator;
-import com.nfcs.singularity.core.repos.RolesRepo;
-import com.nfcs.singularity.core.repos.UsersRepo;
+import com.nfcs.singularity.core.repos.BaseRepo;
+import com.nfcs.singularity.core.repos.BaseRepoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -18,10 +16,10 @@ import org.springframework.web.bind.support.WebRequestDataBinder;
 import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
+import javax.transaction.Transactional;
 import java.util.logging.Logger;
 
 @Controller
@@ -36,44 +34,56 @@ public class BaseController<T extends BaseEntity> {
     GenericWebApplicationContext context;
 
     @Autowired
-    UsersRepo ur;
-
-    @Autowired
-    RolesRepo rr;
-
-    @Autowired
     EntityManager entityManager;
 
     @PostMapping(value = "/save",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String save(@RequestParam String entityClass, @RequestParam(required = false) Long id, Model model, WebRequest request) {
+    @Transactional
+    public String save(@RequestParam String entityClass,
+                       @RequestParam(required = false) Long id,
+                       Model model,
+                       WebRequest request) throws Exception {
+        saveEntity(entityClass, id, model, request, true);
+        return "fragments/entitiesList :: entities-list";
+    }
+
+    @GetMapping("entities")
+    public String listEntities(@RequestParam String entityClass,
+                               @RequestParam(required = false) Long id,
+                               WebRequest request,
+                               Model model) throws Exception {
+        saveEntity(entityClass, id, model, request, false);
+        return "entities";
+    }
+
+    private void saveEntity(String entityClass, Long id, Model model, WebRequest request, boolean save) throws Exception {
         EntityType<T> entityType = (EntityType<T>) entityManager.getMetamodel().getEntities().stream()
                 .filter(e -> e.getName().equals(entityClass)).findFirst().orElse(null);
 
         if (entityType != null) {
-            model.addAttribute("captions", gen.getEntityProperties(entityType.getJavaType()));
 
-            if (entityType.getName().equals("User")) {
-                User user;
+            T entity;
+            BaseRepo br = new BaseRepoImpl(entityType.getJavaType(), entityManager);
 
-                if (id != null) {
-                    user = (User) entityManager.find(entityType.getJavaType(), id);
-                } else {
-                    user = new User();
-                }
+            if (id != null) {
+                entity = (T) br.findById(id).orElse(null);
 
-                bindEntity(request, user);
-                ur.save(user);
-                model.addAttribute("entity", user);
-                model.addAttribute("entities", ur.findAll());
-            } else if (entityType.getName().equals("Role")) {
-                Role role = new Role();
-                bindEntity(request, role);
-                rr.save(role);
+                if (entity == null)
+                    throw new Exception("Entity with id=" + id + " not found");
+            } else {
+                entity = entityType.getJavaType().getConstructor().newInstance();
             }
-        }
 
-        return "fragments/entitiesList :: entities-list";
+            bindEntity(request, entity);
+
+            if (save) {
+                entity = (T) br.save(entity);
+            }
+
+            model.addAttribute("captions", gen.getEntityProperties(entityType.getJavaType()));
+            model.addAttribute("entity", entity);
+            model.addAttribute("entities", br.findAll());
+        }
     }
 
     private BindingResult bindEntity(WebRequest request, BaseEntity entity) {
@@ -81,38 +91,5 @@ public class BaseController<T extends BaseEntity> {
         binder.bind(request);
         binder.validate();
         return binder.getBindingResult();
-    }
-
-    protected String getViewName(Class<T> entityClass) {
-        return entityClass.getSimpleName() + "s";
-    }
-
-    @GetMapping("entities")
-    public ModelAndView listEntities(@RequestParam String entityClass, @RequestParam(required = false) Long id, ModelAndView model) {
-        EntityType<T> entityType = (EntityType<T>) entityManager.getMetamodel().getEntities().stream()
-                .filter(e -> e.getName().equals(entityClass)).findFirst().orElse(null);
-
-        if (entityType != null) {
-            model.addObject("captions", gen.getEntityProperties(entityType.getJavaType()));
-
-            if (entityType.getName().equals("Role")) {
-                model.addObject("entities", rr.findAll());
-                model.addObject("entity", new Role());
-            } else if (entityType.getName().equals("User")) {
-                model.addObject("entities", ur.findAll());
-                User user;
-
-                if (id != null) {
-                    user = (User) entityManager.find(entityType.getJavaType(), id);
-                } else {
-                    user = new User();
-                }
-
-                model.addObject("entity", user);
-            }
-        }
-
-        model.setViewName("entities");
-        return model;
     }
 }
