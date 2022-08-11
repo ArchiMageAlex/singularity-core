97fc0bcbffcd0bad195731c47e94b8de1b684cf8
Alexander <archimagealex@gmail.com>
2022-08-11 10:38:22 +0300

    JKS to Service, MailConfig to Component, RegController fields to setters autowire,

--- api/src/main/java/com/nfcs/singularity/core/service/MailService.java
+++ api/src/main/java/com/nfcs/singularity/core/service/MailService.java
@@ -1,7 +1,9 @@
 package com.nfcs.singularity.core.service;
 
+import com.nfcs.singularity.core.config.MailConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,13 @@ import java.util.logging.Logger;
 @Service
 public class MailService {
     private static Logger LOG = Logger.getLogger(MailService.class.getName());
-    @Autowired
     private JavaMailSender mailSender;
-
     @Value("${spring.mail.username}")
     private String username;
+
+    public MailService(@Autowired MailConfig mailConfig) {
+        this.mailSender = mailConfig.getMailSender();
+    }
 
     public void send(String emailTo, String subject, String message) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -27,7 +31,7 @@ public class MailService {
 
         try {
             mailSender.send(mailMessage);
-        } catch(Exception e) {
+        } catch (Exception e) {
             LOG.log(Level.SEVERE, "Mail does not sent", e);
         }
     }
